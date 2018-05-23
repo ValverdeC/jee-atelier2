04f899494f000a983e5880bf0ea157eb19565e6a
Julien DUCROT <julien.ducrot.mess@gmail.com>
2018-05-23 11:28:59 +0200

    test

--- src/main/java/com/sample/repository/CardRepository.java
+++ src/main/java/com/sample/repository/CardRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.sample.model.Card;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface CardRepository extends JpaRepository<Card, Integer> {
 
-   // @Query("SELECT c FROM Card WHERE c.name = ?")
-   // public Card findByName(String name);
+    @Query("SELECT c FROM Card WHERE c.name = ?")
+    public Card findByName(String name);
 }
